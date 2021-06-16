78eeeef337a89ab0bb8e38afcf55c73515525724
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-06-16 13:58:20 -0300

    title

--- project-1/src/components/Posts/index.jsx
+++ project-1/src/components/Posts/index.jsx
@@ -10,7 +10,7 @@ export default function Posts ({posts}){
             key={post.id}
             cover={post.cover}
             id={post.id}
-            title={post.tle}
+            title={post.title}
             body={post.body}
           />
         ))
